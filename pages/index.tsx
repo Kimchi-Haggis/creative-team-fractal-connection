f75022eef22a55ee07cfbbc0ca1ab1b903535b62
Kimchi-Haggis <kimchihaggis@gmail.com>
2023-01-04 14:45:37 +0000

    [Feat] Member section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Layout from '@layouts/Layout'
+
 import Hero from '@components/section/Hero'
 import About from '@components/section/About'
+import Member from '@components/section/Member'
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
       <About />
+      <Member />
     </Layout>
   )
 }
