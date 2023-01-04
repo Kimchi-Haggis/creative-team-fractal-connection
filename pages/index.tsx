7e5b4e7b2cf4a262bd5e90b14c3a60fac2aff598
Kimchi-Haggis <kimchihaggis@gmail.com>
2023-01-04 14:13:54 +0000

    [Feat] About section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import Hero from '@components/section/Hero'
 import Layout from '@layouts/Layout'
+import Hero from '@components/section/Hero'
+import About from '@components/section/About'
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
+      <About />
     </Layout>
   )
 }
