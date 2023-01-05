9757aaa846d934154dcc53838acd0e64a1e8a3b6
Kimchi-Haggis <kimchihaggis@gmail.com>
2023-01-05 14:51:28 +0000

    [Docs] Hero section

--- components/section/Hero.tsx
+++ components/section/Hero.tsx
@@ -4,35 +4,35 @@ type Props = {}
 
 const Hero = (props: Props) => {
   return (
-    <section>
-      <div className="bg-black text-white py-20">
-        <div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
-          <div className="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
-            <h1 className="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose">TechFest</h1>
-            <h2 className="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">Space : The Timeless Infinity
-            </h2>
-            <p className="text-sm md:text-base text-gray-50 mb-4">Explore your favourite events and
-              register now to showcase your talent and win exciting prizes.</p>
-            <a href="#"
-              className="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent">
-              Explore Now</a>
-          </div>
-          <div className="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
-            <div className="h-48 flex flex-wrap content-center">
-              <div>
-                <img className="mt-28 hidden xl:block" src="https://user-images.githubusercontent.com/54521023/116969935-c13d5b00-acd4-11eb-82b1-5ad2ff10fb76.png" />
-              </div>
-              <div>
-                <img className="mt-24 md:mt-0 p-8 md:p-0"  src="https://user-images.githubusercontent.com/54521023/116969931-bedb0100-acd4-11eb-99a9-ff5e0ee9f31f.png" />
-              </div>
-              <div>
-                <img className="mt-28 hidden lg:block" src="https://user-images.githubusercontent.com/54521023/116969939-c1d5f180-acd4-11eb-8ad4-9ab9143bdb50.png" />
-              </div>
-            </div>
-          </div>
+    <div className="h-screen w-full relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0 bg-cover bg-[url('https://images.unsplash.com/photo-1639353014154-8b9da8815d84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')] lg:bg-black lg:bg-none">
+      <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
+        <svg
+          className="absolute left-0 hidden h-full text-black transform -translate-x-1/2 lg:block"
+          viewBox="0 0 100 100"
+          fill="currentColor"
+          preserveAspectRatio="none slice"
+        >
+          <path d="M50 0H100L50 100H0L50 0Z" />
+        </svg>
+        <img
+          className="hidden lg:flex object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
+          src="https://images.unsplash.com/photo-1639353014154-8b9da8815d84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+          alt=""
+        />
+      </div>
+      <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl lg:my-auto">
+        <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5 text-white">
+          <h2 className="mb-5 font-sans text-5xl font-bold tracking-tight lg:text-7xl">
+            <p>Creative Team</p>
+            <p>Fractal Connection</p>
+          </h2>
+          <h3 className="mt-10 text-2xl lg:text-4xl">
+            <p>"We are not artists </p>
+            <p>but value creators."</p>
+          </h3>
         </div>
       </div>
-    </section>
+    </div>
   )
 }
 
