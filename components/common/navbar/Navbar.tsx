c2b85bf0d42c71e8205e2d17af0897e185f90240
Kimchi-Haggis <kimchihaggis@gmail.com>
2023-01-06 19:01:01 +0000

    [Style] Navbar

--- components/common/navbar/Navbar.tsx
+++ components/common/navbar/Navbar.tsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import FractalConnectionLogo from '@public/fractal-connection-logo.svg'
 
 type Props = {}
 
@@ -10,31 +14,38 @@ const Navbar = (props: Props) => {
     console.log("Nav True");
   }
   return (
-    <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="container flex flex-wrap items-center justify-between mx-auto">
-      <a href="https://flowbite.com/" className="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-      </a>
-      <div className="flex md:order-2">
-        <button onClick={handleNav} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+    <nav className="bg-white px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 mx-auto">
+      <div className="container grid grid-cols-4 items-center mx-auto">
+        <div className='grid items-center col-span-3 md:col-span-2 xl:col-span-3 '>
+          <Link href="/#" className='w-fit'>
+            <Image src={FractalConnectionLogo} className="h-8 w-fit ml-4 lg:ml-20" alt="Logo" />
+          </Link>
+        </div>
+      <div className="w-fit grid md:order-2 col-span-1 mx-auto">
+        <button onClick={handleNav} data-collapse-toggle="navbar-sticky" type="button" className="mx-auto inline-flex items-center p-2 text-sm text-black rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
+          {/* <span className="sr-only">Open main menu</span> */}
+          {nav ?
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          :
+            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+          }
         </button>
       </div>
-      <div className={nav? "items-center justify-between w-full md:flex md:w-auto md:order-1": "hidden md:flex"} id="navbar-sticky">
-        <ul className=" flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <div className={nav? "items-center justify-between w-full md:flex md:w-auto md:order-1 col-span-4": "hidden md:flex"} id="navbar-sticky">
+        <ul className="text-xl flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 mx-auto text-black">
           <li>
-            <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+            <a href="#" className="block py-2 pl-3 pr-4 bg-white  md:bg-transparent md:p-0" aria-current="page">Home</a>
           </li>
           <li>
-            <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+            <a href="#" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0">About</a>
           </li>
           <li>
-            <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+            <a href="#" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0">Members</a>
           </li>
           <li>
-            <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+            <a href="#" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0">Contact</a>
           </li>
         </ul>
       </div>
